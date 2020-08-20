d6f7e28d9b706642e605a57429821a6fbed2ac0c
Alvaro Monasterio <a.monasterio.a@gmail.com>
2020-08-20 07:53:09 -0500

    Add project files.

---
+++ SuperHeroDB/Scripts/service.js
@@ -0,0 +1,12 @@
+﻿function GetAllHeroes() {
+    $.ajax({
+        url: "Service/SuperHeroesService.svc/GetAllHeroes",
+        type: "GET",
+        dataType: "json",
+        success: function (result) {
+            heroes = result;
+            drawHeroTable(result)
+        }
+
+    })
+}
---
+++ SuperHeroDB/Service/SuperHeroService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+
+namespace SuperHeroDB.Service
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class SuperHeroService
+    {
+        [OperationContract,WebGet(ResponseFormat =WebMessageFormat.Json)]
+       public List<SuperHero> GetAllHeroes()
+        {
+            return Data.SuperHeroes;
+        }
+    }
+}
